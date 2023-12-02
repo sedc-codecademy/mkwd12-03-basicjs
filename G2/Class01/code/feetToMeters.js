8eebe910a8363f8891027748da39d4bf904ef63e
Basho123 <jamandilovski@gmail.com>
2023-12-02 12:23:35 +0100

    class 01 added

---
+++ G2/Class01/code/feetToMeters.js
@@ -0,0 +1,5 @@
+let conversionFactor = 0.3048;
+let lengthInFeet = 15;
+
+let convertedValue = lengthInFeet * conversionFactor;
+console.log("The length expressed in meters is: " + convertedValue);
---
+++ G2/Class01/code/rectangleArea.js
@@ -0,0 +1,11 @@
+let a = prompt("Enter side a");
+let b = prompt("Enter side b");
+
+console.log(a);
+console.log(typeof(a));
+
+let parsedA = parseInt(a);
+let parsedB = parseInt(b);
+let area = parsedA*parsedB;
+
+console.log("The area is: "+ area);
