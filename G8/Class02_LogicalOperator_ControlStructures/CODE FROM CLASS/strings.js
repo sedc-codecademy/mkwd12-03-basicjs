8470cbc3c69d5af3e8023f07733a684d89ea76e7
amitkovska <aleksandra.mitkovska@mkhost.com.mk>
2023-12-05 09:58:03 +0100

    Class02_LogicalOperators&ControlStructures

---
+++ G8/Class02_LogicalOperator_ControlStructures/CODE FROM CLASS/strings.js
@@ -0,0 +1,20 @@
+//CONCATINATION
+var firstName="Aleksandra";
+var lastName="Mitkovska"
+
+var fullName= firstName + " " + lastName;
+console.log(fullName);
+
+
+//INTERPOLATION
+var betterFullName=`My name is ${firstName} ${lastName}`;
+console.log(betterFullName);
+
+//ESCAPING CHARACTER WITH \
+var address="\"Dame Gruev\" 1/2/8";
+console.log(address);
+
+var firstName=null;
+console.log(firstName);
+var fullName= firstName + " " + lastName;
+console.log(fullName);
