91b2d4a1c0e2520741288a97f107690d75fcfe38
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-15 20:17:03 +0100

    part 2 done

--- G1/Class06 - exercises/script.js
+++ G1/Class06 - exercises/script.js
@@ -40,30 +40,74 @@ console.log("Kiril has an avg grade of:", kirilAvgGrade)
  in the list
 */
 
-// let availableClothing = ['pants', 'shirt', 'sneakers', 'sandals'];
-// for (let item of availableClothing) {
-//     if (clothing === item) {
-//     }
-// }
-
+// The main function that executes the whole logic
 function shopping() {
-    let chosenTypeOfClothing;
+    let chosenTypeOfClothing; // info of selected value for type of clothing - gets populated ONLY if the value is proper (shirt, pants...)
+    let chosenTypeOfColor;
     
+    // Execute the code UNTIL the user HASN'T selected proper type of clothing
     while(!chosenTypeOfClothing) {
+        // clothing will store the answer for the question for clothing type
         let clothing = prompt('What type of clothing do you want to buy?');
 
+        // Decide if imputed clothing is available or not
         switch (clothing) {
+            // There are two possible cases:
+            // 1. imputed clothing is one of the following values
             case 'pants':
             case 'shirt':
             case 'sneakers':
             case 'sandals':
+                // if it's one of the following values, set it as value to
+                // chosenTypeOfClothing which will also stop the while iteration
                 chosenTypeOfClothing = clothing
-                break;
+                break; // STOP execution
             default:
+                // Provide info that clothing is not available
                 alert(`We don't have ${clothing}, please choose another clothing type.`)
-        }
+        }       
     }
+
+    while(!chosenTypeOfColor) {
+        // clothing will store the answer for the question for clothing type
+        let color = prompt(`What type of color do you want the ${chosenTypeOfClothing} to be in?`);
+
+        // Decide if imputed clothing is available or not
+        switch (color) {
+            // There are two possible cases:
+            // 1. imputed clothing is one of the following values
+            case 'white':
+            case 'black':
+            case 'yellow':
+            case 'green':
+                // if it's one of the following values, set it as value to
+                // chosenTypeOfClothing which will also stop the while iteration
+                chosenTypeOfColor = color
+                break; // STOP execution
+            default:
+                // Provide info that clothing is not available
+                alert(`We don't have ${color}, please choose another color.`)
+        }       
+    }
+    
     
 }
 
 shopping();
+
+// Using combination of if and for-of instead of switch
+/*
+    while(!chosenTypeOfClothing) {
+        let clothing = prompt('What type of clothing do you want to buy?');
+        let availableClothing = ['pants', 'shirt', 'sneakers', 'sandals'];
+        for (let item of availableClothing) {
+            if (clothing === item) {
+                chosenTypeOfClothing = clothing
+                break;
+            }
+        }
+        if (!chosenTypeOfClothing) {
+            alert(`We don't have ${clothing}, please choose another clothing type.`)
+        }        
+    }
+*/
