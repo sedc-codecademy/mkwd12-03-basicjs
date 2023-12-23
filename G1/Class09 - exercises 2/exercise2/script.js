5a61b8a254f9a1afd6db8a8ded867ba7f082c648
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-23 09:59:30 +0100

    Exercises

---
+++ G1/Class09 - exercises 2/exercise2/script.js
@@ -0,0 +1,18 @@
+/*
+
+Write a JavaScript program called PhoneBook. 
+You should have three input fields in the html,
+one for firstName, lastName and phoneNumber.
+After entering the values into each of them,
+on clicking on a “Save” button, you should
+display the newly added contact in a table
+below the inputs. Make sure the user is entering
+an appropriate values in the fields!
+
+Bonus: Add column with delete and edit button
+so that you can edit or delete a contact! Try
+to save the newly added contact as an object
+(literal or constructor function its up to you)
+and add it to an array!
+ 
+*/
