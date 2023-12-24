a08ec374d96fb04bd3c20fdfb741eaff7336253a
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-24 15:22:37 +0100

    Fix exercises

--- G1/Class09 - exercises 2/exercise2/script.js
+++ G1/Class09 - exercises 2/exercise2/script.js
@@ -9,9 +9,7 @@ display the newly added contact in a table
 below the inputs. Make sure the user is entering
 an appropriate values in the fields!
 
-Bonus: Add column with delete and edit button
-so that you can edit or delete a contact! Try
-to save the newly added contact as an object
+Bonus: Try to save the newly added contact as an object
 (literal or constructor function its up to you)
 and add it to an array!
 
@@ -42,74 +40,45 @@ Task breakdown:
 let users = [];
 
 // Selectors
-let firstNameInput = document.getElementById('firstName')
-let lastNameInput = document.getElementById('lastName')
-let phoneNumberInput = document.getElementById('phoneNumber')
-let addButton = document.getElementById('add')
-let tableBody = document.getElementById('data')
-
-// Functionality
-// ?
+let firstNameInput = document.getElementById("firstName");
+let lastNameInput = document.getElementById("lastName");
+let phoneNumberInput = document.getElementById("phoneNumber");
+let addButton = document.getElementById("add");
+let tableBody = document.getElementById("data");
 
 // Events
-addButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    
-    // - Get the values from inputs
-    let firstName = firstNameInput.value
-    let lastName = lastNameInput.value
-    let phoneNumber = phoneNumberInput.value
-    
-    // - Validate inputs
-    if (!firstName || !lastName || !phoneNumber) {
-        alert('All inputs are required!')
-        return;
-    }
-    // - Create an object where the values are the values from the inputs
-    let user = {
-        firstName: firstName, // Joe
-        lastName: lastName, // Doe
-        phoneNumber: phoneNumber // 43128321
-    }
-    // - Add newly created object to array
-    users.push(user);
+addButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    // - Create a table row from the values of the object
-    const tableRow = `
+  // - Get the values from inputs
+  let firstName = firstNameInput.value;
+  let lastName = lastNameInput.value;
+  let phoneNumber = phoneNumberInput.value;
+
+  // - Validate inputs
+  if (!firstName || !lastName || !phoneNumber) {
+    alert("All inputs are required!");
+    return;
+  }
+  // - Create an object where the values are the values from the inputs
+  let user = {
+    firstName: firstName, // Joe
+    lastName: lastName, // Doe
+    phoneNumber: phoneNumber, // 43128321
+  };
+  // - Add newly created object to array
+  users.push(user);
+
+  // - Create a table row from the values of the object
+  const tableRow = `
         <tr>
             <td>${users.length}</td>
             <td>${user.firstName}</td>
             <td>${user.lastName}</td>
             <td>${user.phoneNumber}</td>
-            <td>
-                <button type="button" id="${user.phoneNumber}">Delete</button>
-            </td>
         </tr>
-    `
+    `;
 
-    // - Insert the table row to the table body
-    tableBody.innerHTML += tableRow
-
-    let firstDeleteBtn = document.getElementById(user.phoneNumber)
-    
-    firstDeleteBtn.addEventListener('click', function (event) {
-        // Find if the user in the array
-        let usersPhoneNumber = event.target.id;
-        let foundUser;
-
-        for (let user of users) {
-            if (user.phoneNumber === usersPhoneNumber) {
-                foundUser = user
-            }
-        }
-
-        // Validate user existence
-        if (!foundUser) {
-            alert('No such user!')
-            return
-        }
-
-        // Remove the user from the table
-        tableBody.innerHTML = ''
-    })
-})
+  // - Insert the table row to the table body
+  tableBody.innerHTML += tableRow;
+});
--- G1/Class09 - exercises 2/exercise3/script.js
+++ G1/Class09 - exercises 2/exercise3/script.js
@@ -1,54 +1,53 @@
 // Data
 let todoData = [
-    {
-        name: 'Wash car',
-        isCompleted: true
-    },
-    {
-        name: 'Buy car',
-        isCompleted: false
-    }
-]
+  {
+    name: "Wash car",
+    isCompleted: true,
+  },
+  {
+    name: "Buy car",
+    isCompleted: false,
+  },
+];
 
 // Selected Elements
-let list = document.getElementById('todo-list')
-let submitBtn = document.getElementById('submit')
-let input = document.getElementById('todo-input')
+let list = document.getElementById("todo-list");
+let submitBtn = document.getElementById("submit");
+let input = document.getElementById("todo-input");
 
 // Functionality
 function renderListItems(todos) {
-    list.innerHTML = ''
+  list.innerHTML = "";
 
-    for (let todo of todos) {
-        list.innerHTML += `<li id="${todo.name}">
-        <input type="checkbox" />
+  for (let todo of todos) {
+    list.innerHTML += `<li id="${todo.name}">
         ${todo.name}
-        </li>`
+        </li>`;
 
-        if (todo.isCompleted) {
-            let listItem = document.getElementById(todo.name)
+    if (todo.isCompleted) {
+      let listItem = document.getElementById(todo.name);
 
-            listItem.style.textDecoration = 'line-through'
-        }
+      listItem.style.textDecoration = "line-through";
     }
+  }
 }
 
-renderListItems(todoData)
+renderListItems(todoData);
 
 // Events
-submitBtn.addEventListener('click', function (event) {
-    event.preventDefault()
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    // Take the inputs value
-    let todo = {
-        name: input.value,
-        isCompleted: false
-    }
+  // Take the inputs value
+  let todo = {
+    name: input.value,
+    isCompleted: false,
+  };
 
-    // Add the new value to the todo list
-    todoData.push(todo)
-    // Render all items to the HTML list
-    renderListItems(todoData)
-    // Clear the input value
-    input.value = '';
-})
+  // Add the new value to the todo list
+  todoData.push(todo);
+  // Render all items to the HTML list
+  renderListItems(todoData);
+  // Clear the input value
+  input.value = "";
+});
