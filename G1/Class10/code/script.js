316d433a154da8a5491fb24390d8a07717022633
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-26 15:51:02 +0100

    add comments

--- G1/Class10/code/script.js
+++ G1/Class10/code/script.js
@@ -23,30 +23,37 @@ $(document).ready(function () {
   //   console.log(firstSection);
   //   let lastSection = $("section").last();
   //   console.log(lastSection);
-  // How we get HTML value
+  // How we get HTML value (equivalent to innerHTML)
   //   let firstSectionHTML = $("section").first().html();
   //   console.log(firstSectionHTML);
-  // How we change HTML value
+  // How we change HTML value  (equivalent to innerHTML = "SEGA NESTO DRUGO")
   //   $("section").first().html(`SEGA NESTO DRUGO`);
-  // How we change text
+  // How we change text value (equivalent to innerText)
   //   let headingOne = $("h1").first().text();
-  //   console.log(headingOne);
+  //   console.log(headingOne); (equivalent to innerText = "jQuery e mnogu cool")
   //   $("h1").first().text("jQuery e mnogu cool");
 
+  // Manipulating CSS with jQuery
+  // This is setting the style attribute of the element
+  // Equivalent to document.querySelector("h1").style.color = "red";
   //   $(".red").first().css("color", "red");
   //   $(".red").first().css("background-color", "gray");
 
+  // Used to add/remove/toggle classes
   // document.querySelector('h1').classList
   //   $("h1").first().addClass("black-border");
   //   $("h1").first().removeClass("gray-border");
   //   $("h1").first().toggleClass("white-border");
 
+  // Events in jQuery
   //   $("#show-text").click(function () {
+  // get the value of the input
   //     let inputValue = $("input").first().val();
 
   //     $("#input-showcase").first().text(inputValue);
   //   });
 
+  // show/hide elements with jQuery
   $("#show-man-c").click(function () {
     //   With plain/vanilla JS
     // document.querySelector("#man-c").style.display = "block";
