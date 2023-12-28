7e9738c46778ccea4953fabd01654ffbc4c88544
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-28 19:20:33 +0100

    exercise 2 requirements

---
+++ G2/Class11/exercise2/script.js
@@ -0,0 +1,11 @@
+/* Make a TODO app for yourself. 
+You should have only one input and “Add” button.
+After clicking on the Add button, display the
+newly added task that you need to do in an unordered
+list. Each new todo should be saved into an array.
+Bonus: Add checkbox to each of the tasks in the 
+unordered list, and after checking a task, it 
+should cross trough the text! 
+Try making the ToDo an object that needs to
+have two properties: Name and isCompleted properties
+*/
