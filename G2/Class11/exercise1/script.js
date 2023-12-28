89148bbb8987db21363b72280f2597fbcc7b410b
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-12-28 18:00:46 +0100

    add exercise 1

---
+++ G2/Class11/exercise1/script.js
@@ -0,0 +1,13 @@
+/*
+ === Create a shopping app ===
+ 1. App accepts an input of clothing: shirt, pants, sneakers,
+ sandals - any other type would ask the user to add a
+ proper value
+ 2. App accepts an input of color: white, black, yellow, green
+ - any other color would ask the user to add a proper color
+ 3. App will ask the user to continue shopping or not
+ - if the user continues to shop, we can choose additional
+ clothing and colors
+ 4. App will showcase the bought clothing with it's color
+ in the list
+*/
